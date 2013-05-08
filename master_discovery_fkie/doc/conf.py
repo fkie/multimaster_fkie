cc48142b49329175834a560cec4f327522d29a97
Alexander Tiderko <Alexander.Tiderko@googlemail.com>
2013-05-08 07:35:30 +0200

    module path added to documentation sys.path

--- master_discovery_fkie/doc/conf.py
+++ master_discovery_fkie/doc/conf.py
@@ -20,7 +20,7 @@ import roslib; roslib.load_manifest('master_discovery_fkie')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../src/.'))
 
 
 # -- General configuration -----------------------------------------------------
