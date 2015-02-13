7aafa5ad03e7291ca62911c17b9bdde7a1a64d38
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2015-02-13 11:54:28 +0100

    node_maanger_fkie: removed sync+AnyMsg option, it is now sync with all messages

--- node_manager_fkie/src/node_manager_fkie/sync_dialog.py
+++ node_manager_fkie/src/node_manager_fkie/sync_dialog.py
@@ -125,16 +125,16 @@ class SyncDialog(QtGui.QDialog):
     self.toolButton_SyncAll.setText(QtGui.QApplication.translate("Form", "Sync All", None, QtGui.QApplication.UnicodeUTF8))
     self.toolButton_SyncAll.clicked.connect(self._on_sync_all_clicked)
 
-    self.toolButton_SyncAllAnyMsg = QtGui.QToolButton(self)
-    sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-    sizePolicy.setHorizontalStretch(0)
-    sizePolicy.setVerticalStretch(1)
-    sizePolicy.setHeightForWidth(self.toolButton_SyncAllAnyMsg.sizePolicy().hasHeightForWidth())
-    self.toolButton_SyncAllAnyMsg.setSizePolicy(sizePolicy)
-    self.toolButton_SyncAllAnyMsg.setObjectName("toolButton_SyncAllAnyMsg")
-    self.verticalLayout.addWidget(self.toolButton_SyncAllAnyMsg)
-    self.toolButton_SyncAllAnyMsg.setText(QtGui.QApplication.translate("Form", "Sync all (+AnyMsg)", None, QtGui.QApplication.UnicodeUTF8))
-    self.toolButton_SyncAllAnyMsg.clicked.connect(self._on_sync_all_anymsg_clicked)
+#     self.toolButton_SyncAllAnyMsg = QtGui.QToolButton(self)
+#     sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+#     sizePolicy.setHorizontalStretch(0)
+#     sizePolicy.setVerticalStretch(1)
+#     sizePolicy.setHeightForWidth(self.toolButton_SyncAllAnyMsg.sizePolicy().hasHeightForWidth())
+#     self.toolButton_SyncAllAnyMsg.setSizePolicy(sizePolicy)
+#     self.toolButton_SyncAllAnyMsg.setObjectName("toolButton_SyncAllAnyMsg")
+#     self.verticalLayout.addWidget(self.toolButton_SyncAllAnyMsg)
+#     self.toolButton_SyncAllAnyMsg.setText(QtGui.QApplication.translate("Form", "Sync all (+AnyMsg)", None, QtGui.QApplication.UnicodeUTF8))
+#     self.toolButton_SyncAllAnyMsg.clicked.connect(self._on_sync_all_anymsg_clicked)
 
     self.toolButton_SyncTopicOnDemand = QtGui.QToolButton(self)
     sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
@@ -241,21 +241,21 @@ class SyncDialog(QtGui.QDialog):
     self._interface_filename = None
     self.accept()
 
-  def _on_sync_all_anymsg_clicked(self):
-    self._sync_args = []
-    self._sync_args.append(''.join(['_interface_url:=', "'.'"]))
-    self._sync_args.append(''.join(['_sync_topics_on_demand:=', 'True']))
-    self._sync_args.append(''.join(['_ignore_hosts:=', '[]']))
-    self._sync_args.append(''.join(['_sync_hosts:=', '[]']))
-    self._sync_args.append(''.join(['_ignore_nodes:=', '[]']))
-    self._sync_args.append(''.join(['_sync_nodes:=', '[]']))
-    self._sync_args.append(''.join(['_ignore_topics:=', '[]']))
-    self._sync_args.append(''.join(['_sync_topics:=', '[/*]']))
-    self._sync_args.append(''.join(['_ignore_services:=', '[]']))
-    self._sync_args.append(''.join(['_sync_services:=', '[]']))
-    self._sync_args.append(''.join(['_sync_remote_nodes:=', 'False']))
-    self._interface_filename = None
-    self.accept()
+#   def _on_sync_all_anymsg_clicked(self):
+#     self._sync_args = []
+#     self._sync_args.append(''.join(['_interface_url:=', "'.'"]))
+#     self._sync_args.append(''.join(['_sync_topics_on_demand:=', 'True']))
+#     self._sync_args.append(''.join(['_ignore_hosts:=', '[]']))
+#     self._sync_args.append(''.join(['_sync_hosts:=', '[]']))
+#     self._sync_args.append(''.join(['_ignore_nodes:=', '[]']))
+#     self._sync_args.append(''.join(['_sync_nodes:=', '[]']))
+#     self._sync_args.append(''.join(['_ignore_topics:=', '[]']))
+#     self._sync_args.append(''.join(['_sync_topics:=', '[/*]']))
+#     self._sync_args.append(''.join(['_ignore_services:=', '[]']))
+#     self._sync_args.append(''.join(['_sync_services:=', '[]']))
+#     self._sync_args.append(''.join(['_sync_remote_nodes:=', 'False']))
+#     self._interface_filename = None
+#     self.accept()
 
   def _on_sync_topics_on_demand_clicked(self):
     self._sync_args = []
@@ -414,7 +414,7 @@ class SyncDialog(QtGui.QDialog):
 
   def resetView(self):
     self.toolButton_SyncAll.setVisible(True)
-    self.toolButton_SyncAllAnyMsg.setVisible(True)
+#     self.toolButton_SyncAllAnyMsg.setVisible(True)
     self.toolButton_SyncTopicOnDemand.setVisible(True)
     self.toolButton_SelectInterface.setVisible(True)
     self.interface_field.setVisible(False)
