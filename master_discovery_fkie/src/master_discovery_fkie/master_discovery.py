ac867fe03fa35bff021223277175e763d70705dc
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2017-01-24 09:01:58 +0100

    master_discovery_fkie: removed a not needed import

--- master_discovery_fkie/src/master_discovery_fkie/master_discovery.py
+++ master_discovery_fkie/src/master_discovery_fkie/master_discovery.py
@@ -30,7 +30,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from hgext.convert.convcmd import recode
 from urlparse import urlparse
 import Queue
 import errno
