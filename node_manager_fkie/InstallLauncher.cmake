f071b1a1b5520ee4640d8e6177d63a5569bf7893
Alexander Schaefer <aschaef@cs.uni-freiburg.de>
2018-10-22 13:06:52 -0700

    Improve messages during launcher installation.

--- node_manager_fkie/InstallLauncher.cmake
+++ node_manager_fkie/InstallLauncher.cmake
@@ -1,17 +1,12 @@
 # Install and register the given launcher file.
+message(STATUS "Installing Unity desktop launcher")
 execute_process(COMMAND 
         desktop-file-install --dir=$ENV{HOME}/.local/share/applications ${NODE_MANAGER_LAUNCHER} 
     RESULT_VARIABLE 
         LAUNCHER_INSTALLATION_RESULT
 )
 
-# Print the result.
-if (${LAUNCHER_INSTALLATION_RESULT} EQUAL 0)
-    set(LAUNCHER_INSTALLATION_RESULT success)
-endif (${LAUNCHER_INSTALLATION_RESULT} EQUAL 0)
-
-if (${LAUNCHER_INSTALLATION_RESULT} EQUAL 1)
-    set(LAUNCHER_INSTALLATION_RESULT failure)
-endif (${LAUNCHER_INSTALLATION_RESULT} EQUAL 1)
-
-message(STATUS "Installing Unity desktop launcher: ${LAUNCHER_INSTALLATION_RESULT}.")
+# If an error occurred, print it.
+if (NOT ${LAUNCHER_INSTALLATION_RESULT} EQUAL 0)
+    message(AUTHOR_WARNING "Installing Unity desktop launcher failed")
+endif (NOT ${LAUNCHER_INSTALLATION_RESULT} EQUAL 0)
