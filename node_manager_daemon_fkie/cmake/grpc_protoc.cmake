e2a2dd9e18164501ed636fbd75f276dccf12a53f
Alexander Tiderko <alexander.tiderko@gmail.com>
2018-11-17 12:20:08 +0100

    changed install build and process to pass travis ci

--- node_manager_daemon_fkie/cmake/grpc_protoc.cmake
+++ node_manager_daemon_fkie/cmake/grpc_protoc.cmake
@@ -2,32 +2,32 @@ include(CMakeParseArguments)
 
 macro(generate_grpc)
     find_program(PYTHON python)
-    # set(DEST_DIR "${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_PYTHON_DESTINATION}/generated")
-    set(DEST_DIR "${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME}/generated")
+    set(GRPC_GENERATED_SRC_DIR "${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_PYTHON_DESTINATION}/generated")
+    # set(GRPC_GENERATED_SRC_DIR "${PROJECT_SOURCE_DIR}/src/${PROJECT_NAME}/generated")
     set(PROTO_FILES file launch screen)
     set(GEN_PROTO_FILES "")
     set(ABS_PROTO_PATH "${PROJECT_SOURCE_DIR}/protos")
     # command to create generated directory
     add_custom_command(
-        OUTPUT ${DEST_DIR}
-        COMMAND ${CMAKE_COMMAND} -E make_directory "${DEST_DIR}"
+        OUTPUT ${GRPC_GENERATED_SRC_DIR}
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${GRPC_GENERATED_SRC_DIR}"
         COMMENT "create directory for generated gRPC code: ${CMAKE_CURRENT_SOURCE_DIR}"
     )
-    message(STATUS "destination for gRPC generated code: ${DEST_DIR}")
+    message(STATUS "destination for gRPC generated code: ${GRPC_GENERATED_SRC_DIR}")
     foreach(PROTO_FILE ${PROTO_FILES})
         set(ABS_PROTO_FILE "${ABS_PROTO_PATH}/${PROTO_FILE}.proto")
-        list(APPEND GEN_PROTO_FILES ${DEST_DIR}/${PROTO_FILE}_pb2_grpc.py)
+        list(APPEND GEN_PROTO_FILES ${GRPC_GENERATED_SRC_DIR}/${PROTO_FILE}_pb2_grpc.py)
         message(STATUS "generate gRPC code from ${ABS_PROTO_FILE}")
         add_custom_command(
-            OUTPUT ${DEST_DIR}/${PROTO_FILE}_pb2_grpc.py ${DEST_DIR}/${PROTO_FILE}_pb2.py
-            COMMAND "${PYTHON}" -m grpc_tools.protoc -I${ABS_PROTO_PATH} --python_out=${DEST_DIR}/. --grpc_python_out=${DEST_DIR}/. ${ABS_PROTO_FILE}
-            DEPENDS ${DEST_DIR} ${ABS_PROTO_FILE}
+            OUTPUT ${GRPC_GENERATED_SRC_DIR}/${PROTO_FILE}_pb2_grpc.py ${GRPC_GENERATED_SRC_DIR}/${PROTO_FILE}_pb2.py
+            COMMAND "${PYTHON}" -m grpc_tools.protoc -I${ABS_PROTO_PATH} --python_out=${GRPC_GENERATED_SRC_DIR}/. --grpc_python_out=${GRPC_GENERATED_SRC_DIR}/. ${ABS_PROTO_FILE}
+            DEPENDS ${GRPC_GENERATED_SRC_DIR} ${ABS_PROTO_FILE}
         )
     endforeach()
     # create init file
     add_custom_target(
         TARGET ALL
-        COMMAND ${CMAKE_COMMAND} -E touch "${DEST_DIR}/__init__.py"
+        COMMAND ${CMAKE_COMMAND} -E touch "${GRPC_GENERATED_SRC_DIR}/__init__.py"
         DEPENDS ${GEN_PROTO_FILES}
         COMMENT "Create '__init__.py' for generated gRPC module"
     )
