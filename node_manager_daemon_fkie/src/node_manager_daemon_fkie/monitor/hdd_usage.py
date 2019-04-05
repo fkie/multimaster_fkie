bc069128e101c195923b2e925e3e1675b21be7bb
Alexander Tiderko <Alexander.Tiderko@fkie.fraunhofer.de>
2019-04-05 08:53:23 +0200

    node_manager_daemon_fkie: catch exception to pass the test on travis

--- node_manager_daemon_fkie/src/node_manager_daemon_fkie/monitor/hdd_usage.py
+++ node_manager_daemon_fkie/src/node_manager_daemon_fkie/monitor/hdd_usage.py
@@ -53,21 +53,28 @@ class HddUsage(SensorInterface):
         self._path = settings.param('sysmon/Disk/path', self._path)
 
     def check_sensor(self):
-        hdd = psutil.disk_usage(self._path)
-        diag_level = 0
-        diag_vals = []
-        warn_on_space = hdd.total * (1.0 - self._hdd_usage_warn)
-        diag_msg = 'warn at >%s%% (<%s)' % (self._hdd_usage_warn * 100., sizeof_fmt(warn_on_space))
-        warn_level = warn_on_space
-        if diag_level == DiagnosticStatus.WARN:
-            warn_level = warn_level * 1.1
-        if hdd.free <= warn_on_space:
-            diag_level = DiagnosticStatus.WARN
-            diag_msg = 'Free disk space on log path only %s (warn on <%s)' % (sizeof_fmt(hdd.free), sizeof_fmt(warn_on_space))
-        # print "Percent Disk %.2f" % (hdd.percent), diag_level
-        diag_vals.append(KeyValue(key='Free', value=hdd.free))
-        diag_vals.append(KeyValue(key='Free [%]', value='%.2f' % (100.0 - hdd.percent)))
-        diag_vals.append(KeyValue(key='Path', value=self._path))
+        try:
+            hdd = psutil.disk_usage(self._path)
+            diag_level = 0
+            diag_vals = []
+            warn_on_space = hdd.total * (1.0 - self._hdd_usage_warn)
+            diag_msg = 'warn at >%s%% (<%s)' % (self._hdd_usage_warn * 100., sizeof_fmt(warn_on_space))
+            warn_level = warn_on_space
+            if diag_level == DiagnosticStatus.WARN:
+                warn_level = warn_level * 1.1
+            if hdd.free <= warn_on_space:
+                diag_level = DiagnosticStatus.WARN
+                diag_msg = 'Free disk space on log path only %s (warn on <%s)' % (sizeof_fmt(hdd.free), sizeof_fmt(warn_on_space))
+            # print "Percent Disk %.2f" % (hdd.percent), diag_level
+            diag_vals.append(KeyValue(key='Free', value=hdd.free))
+            diag_vals.append(KeyValue(key='Free [%]', value='%.2f' % (100.0 - hdd.percent)))
+            diag_vals.append(KeyValue(key='Path', value=self._path))
+        except Exception as err:
+            warn_level = DiagnosticStatus.WARN
+            diag_msg = '%s' % err
+            diag_vals.append(KeyValue(key='Free', value="---"))
+            diag_vals.append(KeyValue(key='Free [%]', value="---"))
+            diag_vals.append(KeyValue(key='Path', value=self._path))
 
         # Update status
         with self.mutex:
