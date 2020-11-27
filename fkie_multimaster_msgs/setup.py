5aab817d70012d6f4eeec735b2568d7e217647bb
Alexander Tiderko <alexander.tiderko@fkie.fraunhofer.de>
2020-11-27 17:46:25 +0100

    fixed package name in setup.py

--- fkie_multimaster_msgs/setup.py
+++ fkie_multimaster_msgs/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-   packages=['fkie_multimsater_fkie.grpc']
+   packages=['fkie_multimaster_msgs.grpc']
 )
 
 setup(**d)
